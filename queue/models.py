dd69dc69aa2843f423f8c31511bdf82b8f5780f5
josejapch <josepch@correo.ugr.es>
2016-10-30 19:20:18 +0100

    Modificados los modelos: añadidas primary key, foreign key, ... #20

--- queue/models.py
+++ queue/models.py
@@ -2,24 +2,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-
 class Cola(models.Model):
 	""" Modelo de representacion de una cola.
 	"""
-	codigo_cola = models.CharField(max_length=20, blank=False, null=False)
-	propietario = models.CharField(max_length=20, blank=False, null=False)
+	codigo_cola = models.CharField(default='',max_length=8, blank=False, null=False, unique=True, primary_key=True)
+	propietario = models.CharField(default='',max_length=20, blank=False, null=False)
 	nombre_cola = models.CharField(default='',max_length=20, blank=False, null=False)
 	descripcion = models.CharField(max_length=140, blank=True, null=True)
+	integrantes = models.IntegerField(default=0, editable = False, null=False)
+
 
 	def __unicode__(self): #__str__ en python3
 		return 'Codigo: %s Nombre: %s Propietario: %s Descripcion: %s'%(self.codigo_cola,self.nombre_cola,self.propietario,self.descripcion)
 
+
 class Encolado(models.Model):
 	""" Modelo de representacion de una persona que se ha unido a una cola.
 	"""
-	codigo_cola = models.CharField(max_length=20, blank=False, null=False)
+	codigo_cola = models.ForeignKey('Cola')
 	nick_encolado = models.CharField(max_length=20, blank=False, null=False)
 
+
 	def __unicode__(self): #__str__ en python3
-		return self.nick
+		return self.nick_encolado
