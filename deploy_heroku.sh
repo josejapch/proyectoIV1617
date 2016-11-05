b7654ccea726fc64f4843976e7dff5978994dfc6
josejapch <josepch@correo.ugr.es>
2016-11-05 15:36:49 +0100

    Modificado deploy_heroku.sh para que se compruebe que se ha logueado en heroku antes de ejecutar los comandos de despliegue.

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 heroku login;
-heroku apps:create queueme --region eu;
-git add .;
-git commit -m "Despliegue en Heroku";
-git push heroku master;
-heroku config:set ON_HEROKU=1;
-heroku python manage.py makemigrations;
-heroku python manage.py migrate;
-heroku ps:scale web=1;
+status_login=$?
 
+if [ "$status_login" -ne 0 ]; then
+	echo 'Debe estar registrado en Heroku para hacer el despliegue.\n';
+else
+	heroku apps:create queueme --region eu;
+	git add .;
+	git commit -m "Despliegue en Heroku";
+	git push heroku master;
+	heroku config:set ON_HEROKU=1;
+	heroku run python manage.py makemigrations;
+	heroku run python manage.py migrate;
+	heroku ps:scale web=1;
+fi
